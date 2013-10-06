649e91e0f678287d4a8a8a277e81cc263721f21d
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-06 19:24:55 -0400

    added index.html and js/script.js to prompts_alerts folder

---
+++ prompts_alerts/js/script.js
@@ -0,0 +1,14 @@
+// Prompts and Alerts Activity
+
+/*var userInput = prompt("Enter you year of birth");
+console.log(userInput);*/
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
+var height = prompt("Please enter a height");
+var area = width * height; //calculates with that info
+//console.log(area); //prints it out to console
+
+/*alert("The area of your rectangle is " + area + " sq feet");*/
+
+var result = "The area of your rectangle is " + area + " sq feet";
+alert(result);
