24b66dac58867fc22940e00e0ae85c4ead66c5a1
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-06 18:56:30 -0400

    added index.html/script.js file/styles.css file to js and css folder in Variables and Values folder

---
+++ Values_Variables/js/script.js
@@ -0,0 +1,51 @@
+// JavaScript - Values and Variables -- Bryant, Samantha
+// Using Console.log and alert() Activity
+
+var yearBorn = 1985;
+
+/*
+console.log("Hello World!");
+console.log(yearBorn);
+*/
+
+alert(yearBorn);
+
+
+/* Declaring Variables */
+
+var age;
+
+/* Defining Variables */
+
+var age; //declaration
+age = 28; //definition
+//storage container = value
+
+var yearOfBirth = 1985; //declaring and defining on the same line.
+
+// Variables of Different Types, Numbers, Booleans and Strings
+
+var age = 28; // Number variable
+
+var isStudent = true; // Boolean variable
+
+var firstName = "Samantha"; // String variable
+
+var phrase = "I don't know";
+var phrase = 'I don\'t know \n You\'ll have to speak with the Professor';
+
+// Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+
+/*console.log(muppetNames[0]);*/
+
+muppetNames[1] = "Rizzo"; // changes index #1 to Rizzo
+muppetNames[2] = "Fozzy";
+
+var num = 2;
+
+console.log(muppetNames[num]); // let's me see what's in the index #1
+
+
+// End of lesson
