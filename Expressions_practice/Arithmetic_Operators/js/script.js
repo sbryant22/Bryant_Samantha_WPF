f16f2ac8a1e8044a74102478ac11a0bc0370e7bb
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-07 13:18:07 -0400

    added index.html, js/script.js, css/styles.css and images to Arithmetic_Operators folder

---
+++ Expressions_practice/Arithmetic_Operators/js/script.js
@@ -0,0 +1,13 @@
+// Bryant, Samantha
+
+// Expressions - Arithmetic Operators
+
+
+// area of the triangle is half of the width times the height
+
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log(area);
+
+
