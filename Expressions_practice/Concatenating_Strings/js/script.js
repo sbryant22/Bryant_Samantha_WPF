d8f99a025b9b08ed83c614a90ca08092ca9b2181
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-07 14:46:04 -0400

    added index.html, js/script.js, css/styles.css and images folder

---
+++ Expressions_practice/Concatenating_Strings/js/script.js
@@ -0,0 +1,20 @@
+// Bryant, Samantha
+
+// Expressions - Concatenating Strings
+
+//var firstName = "Samantha";
+//var lastName = "Bryant";
+//var fullName = firstName + " " + lastName;
+
+//console.log(fullName);
+
+// var a = "6";
+// var b = "7";
+
+var a = 6;
+var b = 7;
+
+var result = a + b;
+console.log(result);
+
+
