50130cbfba9a2e89bae1646fc03d54575f8625b1
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-07 17:31:58 -0400

    added example javascript file js/script.js to expressions worksheet folder

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,31 @@
+// Bryant, Samantha - 10/07/13 - Expressions Worksheet
+
+
+// Example
+
+// Calculate the area of a rectangle given the width and height of the rectangle.
+
+// width: 30 - height: 15
+
+var width = 30;
+var height = 15;
+var area = width * height/2;
+var result = width * height/2 + " " +  "sq feet";
+
+console.log(result);
+
+// The circumference of the circle is x
+
+//Calculate the Circumference of a Circle
+
+var radius = 6;
+const PI = 3.14159265;
+
+//Multiply 2, PI and the radius & assign to circ variable.
+
+var circ = radius * 2 * PI;
+
+//print out the output circumference
+
+console.log("the circumference of the circle is" + " " + circ);
+
