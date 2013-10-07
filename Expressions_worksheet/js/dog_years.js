cdccef8bc736cef8bb665b7b8d1658c111e4b403
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-07 17:52:47 -0400

    added dog_years.js to folder for first problem

---
+++ Expressions_worksheet/js/dog_years.js
@@ -0,0 +1,17 @@
+// Bryant, Samantha - 10/07/13 - Expressions Worksheet
+
+
+// Dog Years
+
+// Calculate how old Sparky the Pit Bull is in dog years based on his actual age.
+// We can say that Sparky is 5 years old in human years.
+
+var age = 5;
+
+//Multiply the age by 7
+
+var age = 5 * 7;
+
+//print out the output of Sparky's age
+
+console.log("Sparky is 5 human years old which is" + " " + age + " " + "in dog years");
