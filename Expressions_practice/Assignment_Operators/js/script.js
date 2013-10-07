b3fde5ee192553dc377d93c49ee1a130b0f110a2
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-07 15:00:55 -0400

    added last folder to Expressions_practice folder - Assignment_Operators folder/files

---
+++ Expressions_practice/Assignment_Operators/js/script.js
@@ -0,0 +1,14 @@
+// Bryant, Samantha
+
+// Expressions - Assignment Operators
+
+var a = 3;
+//a += 4; //a = a + 4;
+//a *= 4; //a = a * 4;
+//a /= 4; //a = a / 4;
+//a -= 4; //a = a - 4;
+
+//a ++ ; //a = a + 1 // a += 1
+a -- ; //a = a - 1 // a -= 1
+
+console.log(a);
