1a880b3a71ba1003fd3900001ed113640bc222a6
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-09 13:57:17 -0400

    calculated shopping bill using arrays for shopping_bill.js file

--- Expressions_worksheet/js/shopping_bill.js
+++ Expressions_worksheet/js/shopping_bill.js
@@ -2,4 +2,13 @@
 
 // Average Shopping Bill
 
-// Calculating 
+// Calculating a grocery list of the past 5 weeks using Arrays
+
+var groceryAmount = [100, 125, 134, 105, 122];
+
+var total = groceryAmount[0] + groceryAmount[1] + groceryAmount[2] + groceryAmount[3] +
+    groceryAmount[4];
+
+var weeklyAverageTotal = total/5;
+
+console.log("You have spent a total of $"+ total +" on groceries over 5 weeks. That is an average of $"+  weeklyAverageTotal +" per week.");
