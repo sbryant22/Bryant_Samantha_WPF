c69cf9d038eba4b78ff0e5c5562dfaf727a0b1a3
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-09 12:22:59 -0400

    added slice_pie part 1 javascript file to Expressions_worksheet folder

---
+++ Expressions_worksheet/js/slice_pie1.js
@@ -0,0 +1,27 @@
+// Bryant, Samantha - 10/07/13 - Expressions Worksheet
+
+
+// Slice of Pie Part 1
+
+// Calculate the number of pizza slices, people at the party, and the number of pizzas ordered
+
+var numberOfSlices = 8; // initialize number of slices
+var peopleAtParty = 10; // initialize the amount of people at the party
+var numberOfPizzaOrdered = 5; // initialize the number of pizzas to order
+
+// equation to find the total
+var total = numberOfPizzaOrdered * (numberOfSlices/peopleAtParty);
+
+
+// print out the output of how much pizza
+
+console.log("Each person ate "+ total +" slices of pizza at the party.");
+
+
+
+
+
+
+
+
+
