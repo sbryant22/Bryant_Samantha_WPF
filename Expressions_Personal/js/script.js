555a2c47d9fed9c73d8be97da6a981f9933aab9e
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 11:02:43 -0400

    defined second array and commented for the line

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -9,7 +9,7 @@
 
 var songsPurchased = newArray(4); // Array for songs purchased
 songsPurchased[0] = prompt("Please enter the total dollar amount of songs purchased for that week"); // Total dollar amount for songs purchased
-
+songsPurchased[1] = prompt("Please enter the total dollar amount of songs purchased for following week.") // Total dollar amount for songs purchased for that week
 
 
 
