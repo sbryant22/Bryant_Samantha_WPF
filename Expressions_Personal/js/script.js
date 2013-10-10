0e497e797b1dda4423de81ea044fc41e7cffb145
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 12:52:57 -0400

    adding more definitions to the array for songs purchased

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,7 +7,11 @@
 
 // Givens:
 
-var songsPurchased = [3.87,"week2","week3","week4"]; // Array for songs purchased in increments of weeks
+var songsPurchased = ["week1", "week2", "week3", "week4"]; // Array for songs purchased in increments of weeks
+var week1 = songsPurchased[0]; // Defining that week 1 is the zero songs purchased variable 
+var week2 = songsPurchased[1]; // Defining that week 2 is the first songs purchased variable
+var week3 = songsPurchased[2]; // Defining that week 3 is the second songs purchased variable
+var week4 = songsPurchased[3]; // Defining that week 4 is the third songs purchased variable
 songsPurchased[0] = prompt("Please enter the total dollar amount of songs purchased for first week."); // Total dollar amount for songs purchased for that week
 songsPurchased[1] = prompt("Please enter the total dollar amount of songs purchased for second week."); // Total dollar amount for songs purchased for that week
 songsPurchased[2] = prompt("Please enter the total dollar amount of songs purchased for third week."); // Total dollar amount for songs purchased for that week
@@ -19,8 +23,8 @@ var giftCard = prompt("Please enter a gift card amount (If you have one. If not,
 
 // Equation:
 
-var total = songsPurchased[0] + songsPurchased[1] + songsPurchased[2] + songsPurchased[3]; // add up the amounts of songs purchased
-var monthlyTotal = total/howManyWeeks - giftCard; // after adding and getting a total, multiply it by how many weeks and then subtract a gift card amount if user enters one
+var total = (songsPurchased[0] + songsPurchased[1] + songsPurchased[2] + songsPurchased[3]) - giftCard; // add up the amounts of songs purchased
+var monthlyTotal = total/howManyWeeks; // after adding and getting a total, multiply it by how many weeks and then subtract a gift card amount if user enters one
 
 console.log("You have spent a total of $"+ total +" over the course of "+ howManyWeeks +" weeks. That is an average of $"+ monthlyTotal +" per month.") // print out results for the total for the month
 
