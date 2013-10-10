47307df7eb326902afb6e3436a174fc54d2bfc34
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 14:54:26 -0400

    fixed array addition errors
    fixed up the addition errors that pertained to the array and deleted
    out unwanted comments and/or code.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,11 +7,12 @@
 
 // Givens:
 
-var songsPurchased = ["week1", "week2", "week3", "week4"]; // Array for songs purchased in increments of weeks
-songsPurchased[0] = prompt("Please enter the total dollar amount of songs purchased for first week."); // Total dollar amount for songs purchased for that week
-songsPurchased[1] = prompt("Please enter the total dollar amount of songs purchased for second week."); // Total dollar amount for songs purchased for that week
-songsPurchased[2] = prompt("Please enter the total dollar amount of songs purchased for third week."); // Total dollar amount for songs purchased for that week
-songsPurchased[3] = prompt("Please enter the total dollar amount of songs purchased for fourth week."); // Total dollar amount for songs purchased for that week
+var songsPurchased = new Array(); // Array for songs purchased from ITunes
+songsPurchased[0] = 1.29; // Total dollar amount for songs purchased for that week
+songsPurchased[1] = 2.58; // Total dollar amount for songs purchased for that week
+songsPurchased[2] = 9.00; // Total dollar amount for songs purchased for that week
+songsPurchased[3] = 3.25; // Total dollar amount for songs purchased for that week
+songsPurchased[4] = prompt("Please enter the total dollar amount of songs purchased for the week."); // Total dollar amount for songs purchased for that week
 
 var howManyWeeks = prompt("Please enter the number of weeks."); // the amount of weeks that the user wants to divide the total by
 
@@ -20,7 +21,7 @@ var giftCard = prompt("Please enter a gift card amount (If you have one. If not,
 // Equation:
 
 var total = songsPurchased[0] + songsPurchased[1] + songsPurchased[2] + songsPurchased[3]; // add up the amounts of songs purchased
-var weeklyAverage = (total/howManyWeeks) - giftCard; // after adding and getting a total, multiply it by how many weeks and then subtract a gift card amount if user enters one
+var weeklyAverage = giftCard - (total/howManyWeeks); // after adding and getting a total, multiply it by how many weeks and then subtract a gift card amount if user enters one
 
 console.log("You have spent a total of $"+ total +" over the course of "+ howManyWeeks +" weeks. That is a weekly average of $"+ weeklyAverage +".") // print out results for the total for the month
 
