59a9fbc824b45a119fb306b9fa1788f590d1566d
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 11:13:18 -0400

    added comment for Array expression

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,7 +7,7 @@
 
 // Givens:
 
-var songsPurchased = ["week1","week2","week3","week4"];
+var songsPurchased = ["week1","week2","week3","week4"]; // Array for songs purchased in increments of weeks
 songsPurchased[0] = prompt("Please enter the total dollar amount of songs purchased for first week.") // Total dollar amount for songs purchased for that week
 
 //var songsPurchased = newArray(4); // Array for songs purchased
