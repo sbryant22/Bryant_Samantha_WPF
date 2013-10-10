029afb2b166bf984f4d663920f1a1c8a59139535
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 03:14:59 -0400

    added script.js file to js folder for Expressions_Industry folder
    created script.js file and put in name, date, and assignment name along
    with Industry problem to calculate.

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,10 @@
+// Bryant, Samantha - 10/09/13 - Industry Expression
+
+// This expression represents the web design and development industry
+
+// The formula used for when you are needing to move an image across an area of a web page.
+
+
+
+
+
