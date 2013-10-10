38e04317c31934ab2eb66b858a0ab7cc93be25bb
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 05:12:26 -0400

    Name, date, assignment name and some comments made to script.js file

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,9 @@
+// Bryant, Samantha - 10/10/13 - Wacky Expression
+
+// Calculate how many Twizzlers it would take to sculpt the eiffel tower. A small one of course.
+
+// One Twizzler candy piece is 12in.
+
+// Givens:
+
+
