4cbcf3e868bcc12e5e2e7f1d9c8cbbc31011a284
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 04:43:16 -0400

    changed inches back to pixels

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,9 +7,9 @@
 
 // Givens:
 
-var totalWidth = prompt("Please enter the total width of your web page layout. \nHint: In inches."); // total width of entire web page
+var totalWidth = prompt("Please enter the total width of your web page layout. \nHint: In pixels."); // total width of entire web page
 
-var width = prompt("Please enter the width (in inches) of the section/area you are moving image to."); // the width of the section that you want to move your image to and from.
+var width = prompt("Please enter the width (in pixels) of the section/area you are moving image to."); // the width of the section that you want to move your image to and from.
 
 var positionNum = prompt("Please enter your position number. \nHint: Use the width of the image across area you want and assign the images a number. (1,2,3 etc..)"); // the position number that you want to move the image to
 
@@ -17,4 +17,4 @@ var positionNum = prompt("Please enter your position number. \nHint: Use the wid
 
 var total = (width * positionNum) - totalWidth; // the formula for finding the exact placement for user's image.
 
-console.log("You want to move your image "+ total +" inches."); // prints out the total to the console
+console.log("You want to move your image "+ total +" pixels."); // prints out the total to the console
