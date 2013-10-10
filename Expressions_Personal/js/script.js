7281188483789bb6c2762ea831aeab3267d4c1c8
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 11:41:14 -0400

    added comment to how many weeks variable var howManyWeeks
    also fixed wording on prompt message for fourth week and on the how
    many weeks variable prompt message. Changed the word "add" to "enter".

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -11,9 +11,10 @@ var songsPurchased = ["week1","week2","week3","week4"]; // Array for songs purch
 songsPurchased[0] = prompt("Please enter the total dollar amount of songs purchased for first week."); // Total dollar amount for songs purchased for that week
 songsPurchased[1] = prompt("Please enter the total dollar amount of songs purchased for second week."); // Total dollar amount for songs purchased for that week
 songsPurchased[2] = prompt("Please enter the total dollar amount of songs purchased for third week."); // Total dollar amount for songs purchased for that week
-songsPurchased[3] = prompt("Please enter the total dollar amount of songs purchased for third week."); // Total dollar amount for songs purchased for that week
+songsPurchased[3] = prompt("Please enter the total dollar amount of songs purchased for fourth week."); // Total dollar amount for songs purchased for that week
 
-var howManyWeeks = prompt("Please add how many weeks that you are wanting to calculate."); 
+var howManyWeeks = prompt("Please enter the number of weeks."); // the amount of weeks that the user wants to divide the total by
+
 
 
 
