b0246a5fdb82f41424640c1d55de2ddefec14aca
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 04:07:43 -0400

    fixed issue in the code for script.js file
    fixed the date of the assignment/ fixed code by putting equal signs
    where semicolons were.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,4 +1,4 @@
-// Bryant, Samantha - 10/09/13 - Industry Expression
+// Bryant, Samantha - 10/10/13 - Industry Expression
 
 // This expression represents the web design and development industry
 
@@ -7,9 +7,13 @@
 
 // Givens:
 
-var totalWidth: prompt("Please enter the total width of your web page layout."); // total width of entire web page
-var width: prompt("What is the width of the area/section that you are wanting to move image to?"); // the width of the section that you want to move your image to and from
-var positionNum: prompt("What is the number of it's position? \nHint: Use the width of the image across area you want and assign the images a number"); // the position number that you want to move the image to
+var totalWidth = prompt("Please enter the total width of your web page layout."); // total width of entire web page
 
+var width = prompt("What is the width of the area/section that you are wanting to move image to?"); // the width of the section that you want to move your image to and from.
 
+var positionNum = prompt("What is the number of it's position? \nHint: Use the width of the image across area you want and assign the images a number"); // the position number that you want to move the image to
+
+// Equation:
+
+//var total = (totalWidth) - (width * positionNum);
 
