eafa9ca47726cd2cd029e776f1cb0b1d566aff3b
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 11:12:11 -0400

    redefined first array with prompt message and commented for the line

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -8,6 +8,7 @@
 // Givens:
 
 var songsPurchased = ["week1","week2","week3","week4"];
+songsPurchased[0] = prompt("Please enter the total dollar amount of songs purchased for first week.") // Total dollar amount for songs purchased for that week
 
 //var songsPurchased = newArray(4); // Array for songs purchased
 //songsPurchased[0] = prompt("Please enter the total dollar amount of songs purchased for that week."); // Total dollar amount for songs purchased for that week
