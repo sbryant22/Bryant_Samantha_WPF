876826b9ee137397867ab2cb3b2dea6b1c177e42
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 03:29:45 -0400

    added position number variable to script.js file
    added var positionNum is 5px to the script.js file with comment

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -8,7 +8,7 @@
 
 var totalWidth: 960; // total width of entire web page
 var width: 200; // the width of the section that you want to move your image to and from
-
+var positionNum: 5; // the position number that you want to move the image to
 
 
 
