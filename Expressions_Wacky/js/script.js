064ca15d3d4e2b1291ad4771165c2a891f18b3c9
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-10 05:20:09 -0400

    added a comment to width variable for the Eiffel Tower

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,4 +8,6 @@
 
 var length = prompt("Please enter the length (in inches) of the Twizzler candy you want to use."); // Initialize the length of a piece of Twizzler candy.
 
-var width = prompt("Please add the width (in inches) of the Eiffel Tower you want to create.");
+var width = prompt("Please add the width (in inches) of the Eiffel Tower you want to create."); // Initialize the width of the Eiffel Tower that you want to replicate. Big or small.
+
+
