fa8c5a61d807e1cdec85ab7f1f75d7c873235500
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-13 17:55:23 -0400

    added relational_express.js file to js folder
    Relational Expressions

---
+++ Conditionals_Practice/js/relational_express.js
@@ -0,0 +1,15 @@
+// Bryant, Samantha - Conditionals
+
+// Relational Expressions
+
+var kidHeight = 50;
+var minHeight = 48;
+
+// if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+
