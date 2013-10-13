12bf1dff4fdcc0a4fd530fafc6a0c4519e19b47d
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-13 17:59:59 -0400

    added code to condition_express.js file

--- Conditionals_Practice/js/condition_express.js
+++ Conditionals_Practice/js/condition_express.js
@@ -2,4 +2,15 @@
 
 // Conditions with Expressions
 
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+// if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+
+if(kidHeight + sneakerLift > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
 
