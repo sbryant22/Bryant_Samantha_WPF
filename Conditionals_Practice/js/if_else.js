cf00177ebc4c275d7bd507777593dfa8d28f0f29
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-13 18:07:55 -0400

    added if_else.js file to js folder

---
+++ Conditionals_Practice/js/if_else.js
@@ -0,0 +1,17 @@
+// Bryant, Samantha - Conditionals
+
+// If and else statements
+
+var kidHeight = 52;
+var minHeight = 48;
+
+// if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}else{
+    //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!");
+}
