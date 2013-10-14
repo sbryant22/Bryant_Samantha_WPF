bfcd42d4b7ca1802501e8d01a7aaac71fe84eeae
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-14 15:07:09 -0400

    added ternary.js file to js folder

---
+++ Conditionals_Practice/js/ternary.js
@@ -0,0 +1,29 @@
+// Bryant, Samantha - Conditional Logic
+
+// Ternary Operators
+
+
+// Represents code that we might type if did own conditional statement
+
+if(condition){
+    do if true;
+}else{
+    do if false;
+}
+
+(condition) ? do if true : do if false;
+
+//var gpa = 48;
+
+//if the gpa is over the min 2.0 score, the student can graduate.
+
+/*f(gpa > 2.0){
+    console.log("You can graduate!");
+}else{
+    console.log("GPA is too low!");
+}*/
+
+//(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
+
+
+
