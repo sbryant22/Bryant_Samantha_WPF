5f3d5844760c3a3a67f65b163a762de708235d06
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-14 17:29:47 -0400

    added last_chance.js file to index.html file

---
+++ Conditionals_worksheet/js/last_chance.js
@@ -0,0 +1,16 @@
+// Bryant, Samantha - 10/14/13 - Conditions Worksheet
+
+// Last Chance for Gas!
+
+// You will be passing the last gas station for the next 200 miles, do you need gas now or do you wish to keep going?
+
+// Givens:
+
+// Gas efficiency of the car (in mpg)
+
+//Gauge reading of the gas tank (in %)
+
+//Car's gas tank capacity (in gallons)
+
+
+// Results to print:
