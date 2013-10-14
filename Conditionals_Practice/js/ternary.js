130b56c6c6ec5a577463ad19c87f5da61d4e32d5
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-14 15:13:05 -0400

    Commented out some code and added some more code to js file

--- Conditionals_Practice/js/ternary.js
+++ Conditionals_Practice/js/ternary.js
@@ -5,13 +5,13 @@
 
 // Represents code that we might type if did own conditional statement
 
-if(condition){
+/*if(condition){
     do if true;
 }else{
     do if false;
 }
 
-(condition) ? do if true : do if false;
+(condition) ? do if true : do if false;*/
 
 //var gpa = 48;
 
@@ -25,5 +25,17 @@ if(condition){
 
 //(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
 
+var age = 6;
+var book;
+
+//if the child is under 10, they get Green Eggs and Ham by Dr. Seuss, otherwise they get The Time Machine by HG Wells
+
+/*if(age < 10){
+    book = "Green Eggs and Ham";
+}else{
+    book = "The Time Machine";
+}*/
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
 
 
