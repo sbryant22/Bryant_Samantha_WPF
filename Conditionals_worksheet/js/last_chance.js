697596f2a6a130e544cff7735367ba399d923e3d
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-14 17:33:08 -0400

    added variables to last_chance.js file

--- Conditionals_worksheet/js/last_chance.js
+++ Conditionals_worksheet/js/last_chance.js
@@ -6,11 +6,16 @@
 
 // Givens:
 
+var carMpg = 25;
 // Gas efficiency of the car (in mpg)
 
+var fuelGauge = 50;
 //Gauge reading of the gas tank (in %)
 
+var gasTank = 10;
 //Car's gas tank capacity (in gallons)
+
+
 
 
 // Results to print:
