506e8e44635183d0c3a2f71ea4134fe7c6b32713
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-16 18:20:13 -0400

    added grade_letter.js file to js folder

---
+++ Conditionals_worksheet/js/grade_letter.js
@@ -0,0 +1,19 @@
+// Bryant, Samantha - 10/14/13 - Conditions Worksheet
+
+// Grade Letter Calculator
+
+/* A student earns a number grade at the conclusion of a course at Full Sail. Determine the appropriate
+letter grade for that number using conditional statements.
+ */
+
+/* Full Sail grade scale:
+    A+ = 95-100
+    A = 90-94
+    B+ = 85-89
+    B = 80-84
+    C+ = 76-79
+    C = 73-75
+    D = 70-72
+    F = 0-69
+ */
+
