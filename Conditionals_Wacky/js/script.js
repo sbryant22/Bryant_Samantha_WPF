c91a7f02cf9e5e380e7d02eec59b876a495f0f13
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-17 05:29:07 -0400

    added content to description at top of page

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,5 +1,7 @@
 // Bryant, Samantha - 10/17/13 - Conditionals Assignment - Wacky
 
-// How many steps does it take to get to the pot of gold at the end of a Rainbow?
+/* How many steps would it take to get to the pot of gold at the end of a Rainbow and how many colors are in the rainbow?
+Rainbow has to have all colors present for pot of gold to appear. */
+
 
 // Given:
