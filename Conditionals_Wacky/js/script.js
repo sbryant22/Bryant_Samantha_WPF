9a46a9ef8bf6c40881126c17163ee53056826c55
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-17 07:55:26 -0400

    Changed description of Wacky Conditional at top of page

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,6 +1,7 @@
 // Bryant, Samantha - 10/17/13 - Conditionals Assignment - Wacky
 
-/* How many steps would it take to get to the pot of gold at the end of a Rainbow and how many colors are in the rainbow?
+/* To get the pot of gold at the end of a rainbow, first, you have to take more than 100 steps towards either side of rainbow.
+Also, there needs to be a rainbow actually present for it to appear.
 Rainbow has to have all colors present for pot of gold to appear. */
 
 
