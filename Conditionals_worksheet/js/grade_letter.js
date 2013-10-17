549b9250dd326f8ccc75c4ec66dbedb1aaa9e259
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-17 02:04:59 -0400

    added else if statements and comments

--- Conditionals_worksheet/js/grade_letter.js
+++ Conditionals_worksheet/js/grade_letter.js
@@ -34,11 +34,30 @@ var grades = prompt ("Please enter the number grade (in percent) that you have r
 
 // Condition and result to print out:
 
-if(grades > gradeHighA){
-    //if number grade is greater than or equal to 95%, you have an A+
+if(grades > 95){
+    //if number grade is greater than 95%, you have an A+
     console.log("You have a "+ grades +", which means you have earned a(n) "+ gradeHighA +" in the class!");
-}else{
+}else if(grades > 90 && grades < 94){
+    //if number grade is greater than 90% and less than 94%, you have an A
     console.log("You have a "+ grades +", which means you have earned a(n) "+ gradeAverageA +" in the class!");
+}else if(grades > 85 && grades < 89){
+    //if number grade is greater than 85% and less than 89%, you have a B+
+    console.log("You have a "+ grades +", which means you have earned a(n) "+ gradeHighB +" in the class!");
+}else if(grades > 80 && grades < 84){
+    //if number grade is greater than 80% and less than 84%, you have a B
+    console.log("You have a "+ grades +", which means you have earned a(n) "+ gradeAverageB +" in the class!");
+}else if(grades > 76 && grades < 79){
+    //if number grade is greater than 76% and less than 79%, you have a C+
+    console.log("You have a "+ grades +", which means you have earned a(n) "+ gradeHighC +" in the class!");
+}else if(grades > 73 && grades < 75){
+    //if number grade is greater than 73% and less than 75%, you have a C
+    console.log("You have a "+ grades +", which means you have earned a(n) "+ gradeAverageC +" in the class!");
+}else if(grades > 70 && grades < 72){
+    //if number grade is greater than 70% and less than 72%, you have a D
+    console.log("You have a "+ grades +", which means you have earned a(n) "+ gradeD +" in the class!");
+}else if(grades < 69){
+    //if number grade is less than 69%, you have an F
+    console.log("You have a "+ grades +", which means you have earned a(n) "+ gradeF +" in the class!");
 }
 
 
