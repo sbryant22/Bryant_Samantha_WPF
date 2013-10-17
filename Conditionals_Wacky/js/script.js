d77dc76b3110c69ec562532ca56a434868e96e58
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-17 07:31:01 -0400

    added second variable (how many colors) to console.log sentence

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -19,7 +19,7 @@ var howManyColors = prompt("Please enter how many colors you see in the rainbow.
 // If Else Statement and results to print to console:
 
 if(steps > 100 && howManyColors >= 7){
-    console.log("You have walked a total of "+ steps +" steps, so you get the Pot of Gold!");
+    console.log("You have walked a total of "+ steps +" steps and "+ howManyColors +" colors are present, so you get the Pot of Gold!");
     // If the user has taken more than 100 steps towards the end of the rainbow or there are greater than or equal to 7 colors present than the user will find the Pot of Gold.
 }else{
     console.log("Without a rainbow, there is no Pot of Gold!");
