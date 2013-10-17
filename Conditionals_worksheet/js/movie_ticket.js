29f9e09a46b995a4da6b3652210f63bd16485b93
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-17 01:26:16 -0400

    added movie_ticket.js file to js folder

---
+++ Conditionals_worksheet/js/movie_ticket.js
@@ -0,0 +1,10 @@
+// Bryant, Samantha - 10/17/13 - Conditions Worksheet
+
+// Movie Ticket Price
+
+/* The local movie theater in town charges $12.00 a ticket unless you are a senor (55 or older).
+If a senor or a child under the age of ten, you get a discounted price of $7.00. Also, if you are viewing a movie
+around 3pm and 5pm you can also get the discounted price.
+ */
+
+// Given:
