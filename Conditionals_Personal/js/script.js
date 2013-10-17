c3a3e48a3e7184bb0603239c444c21ad9b45ce18
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-17 02:44:39 -0400

    added description of Personal Conditionals to script.js file

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,9 @@
 // Bryant, Samantha - 10/17/13 - Conditionals Assignment - Personal
 
+// Traffic Patterns
 
+/* If leaving home between 7:00am and 7:20am to get to work by 8:30am, you will make it. If leaving after 7:30am
+but before 8:00am, you will not make it in time due to traffic.
+ */
+
+// Givens:
