3641768c10a5ae77dfdc771a9e0b06c9f21ca91b
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-21 15:01:15 -0400

    added code to var_scope.js file

--- Functions_practice/js/var_scope.js
+++ Functions_practice/js/var_scope.js
@@ -3,3 +3,14 @@
 // Variable Scope
 
 
+var width = 5;
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    //console.log(area);
+}
+
+console.log(width);
+calcArea();
