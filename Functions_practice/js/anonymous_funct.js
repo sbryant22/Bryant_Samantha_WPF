eb74d147e5bea3f513af1465c668f249aadb8362
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-21 15:34:04 -0400

    added anonymous_funct.js file w/code to js folder

---
+++ Functions_practice/js/anonymous_funct.js
@@ -0,0 +1,24 @@
+// Bryant, Samantha - Functions Practice
+
+// Anonymous Function
+
+function functionName(){
+
+}
+
+var functionName = function(){
+    //code to run
+}
+
+functionName();
+
+
+var calcArea = function(width, height){ //defining
+    //code the function runs
+    var area = width * height;
+    return area;
+}
+
+var a = calcArea(20, 30); //invoking
+
+console.log(a);
