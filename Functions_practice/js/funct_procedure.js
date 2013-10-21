351eebe0d1e149659420853c54e2d3efb3eb2732
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-21 15:25:45 -0400

    added funct_procedure.js file w/code to js folder

---
+++ Functions_practice/js/funct_procedure.js
@@ -0,0 +1,17 @@
+// Bryant, Samantha - Functions Practice
+
+// Function vs. Procedures
+
+//This is a function
+function calcAreaF(width, height){
+    var area = width * height;
+    return area;
+}
+
+//This is a procedure
+function calcAreaP(width, height){
+    var area = width * height;
+    console.log(area); // no return
+}
+
+
