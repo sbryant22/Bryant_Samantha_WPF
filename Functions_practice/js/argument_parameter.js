06f318083ed1420fc5147ade2b87b89107b9d1fb
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-21 15:14:48 -0400

    added argument_parameter.js file w/code to js folder

---
+++ Functions_practice/js/argument_parameter.js
@@ -0,0 +1,21 @@
+// Bryant, Samantha - Functions Practice
+
+// Arguments and Parameters
+
+
+//calcArea(30, 20); // 30 is first argument, 20 is second argument
+
+/*function calcArea(w, h){ // w = 30, h = 20 - w is first parameter, h is second parameter
+    var area = w * h;
+    console.log(area);
+}*/
+
+function dogYears(age){ //parameters
+    //var age = 4;
+    var dogYears = age * 7;
+    console.log("Sparky is "+ dogYears +" years old.");
+}
+
+var age1 = 4;
+dogYears(age1); //arguments
+dogYears(5);
