18681321d34895e4a536ac78859bbd4d60e674ca
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-21 13:22:46 -0400

    added basic.js file to js folder

---
+++ Functions_practice/js/basic.js
@@ -0,0 +1,16 @@
+// Bryant, Samantha - Functions Practice
+
+// Basic Function Structure
+
+
+function outptMsg(){
+    console.log("Hello World!");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
