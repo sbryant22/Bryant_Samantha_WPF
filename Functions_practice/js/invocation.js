17f735cc8baf38be17135f0ab7882e95bf24d8b5
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-21 14:57:30 -0400

    added invocation.js file to js folder with code

---
+++ Functions_practice/js/invocation.js
@@ -0,0 +1,19 @@
+// Bryant, Samantha - Functions Practice
+
+// Function Invocation
+
+
+function outptMsg(){
+    console.log("Hello World!");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+calcArea();
+calcArea();
+calcArea(); // makes the function reusable and repeatable
