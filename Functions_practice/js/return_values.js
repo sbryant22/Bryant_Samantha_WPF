5ef514e919d9f2292231228146e443e034571e85
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-21 15:21:01 -0400

    added return_values.js file w/code to js folder

---
+++ Functions_practice/js/return_values.js
@@ -0,0 +1,13 @@
+// Bryant, Samantha - Functions Practice
+
+// Returning Values
+
+
+var total = calcArea(30, 20);
+
+function calcArea(w, h){
+    var area = w * h;
+    return area; // function is spitting the info out
+ }
+
+console.log(total);
