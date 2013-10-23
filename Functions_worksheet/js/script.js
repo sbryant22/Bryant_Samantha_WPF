9c3ed5e7f5e967fb3e44769f524b3e37d130edaa
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-23 18:30:22 -0400

    added console.log to print out the circumference to console

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -9,3 +9,5 @@ function circumference(radius){ //parameter is the radius
     var circumference = (2 * 3.14) * radius; // Circumference = 2(pi) * radius - equation for finding the circumference of a circle
     return circumference; // returning the circumference - this function is putting the info out there
 }
+
+console.log("The circumference of the circle is "+ circumference +"");
