eff1ec0b909f07469b1c3b1d9129d1dbe479bec0
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-24 15:29:56 -0400

    added comment to first given variable

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -7,3 +7,5 @@
 // Given:
 
 var duplicate = prompt("Please enter the number of times you would like your image to repeat.");
+//Asking for the number of times the user would need to duplicate their shape for Star Burst design
+
