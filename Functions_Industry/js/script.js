d3f12a80fe06f7f25d231c1ad5a5ae2983f558bd
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-24 16:31:15 -0400

    revised a comment for function line
    also edited the starBurstDesign variable to just say starBurst

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -19,9 +19,9 @@ var total = starBurst(duplicate, rotate, 11);
 
 // Function:
 
-function starBurst(duplicate, rotate, rotatingAmount){ //parameters for starburst - 2 parameters for this one
+function starBurst(duplicate, rotate, rotatingAmount){ //parameters for starburst
     var rotatingAmount = 11; //the number of times to rotate the shape to make the Star Burst design
-    var starBurstDesign = (duplicate + rotate) * rotatingAmount; // Equation for making a starburst design
+    var starBurst = (duplicate + rotate) * rotatingAmount; // Equation for making a starburst design
 }
 
 // Ternary Statement and print to console:
