6c6e0a22ae27ce51c72bf913e96f7a18c95f3519
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-24 15:35:40 -0400

    added comment to second given variable

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -10,3 +10,5 @@ var duplicate = prompt("Please enter the number of times you would like your ima
 //Asking for the number of times the user would need to duplicate their shape for Star Burst design
 
 var rotate = prompt("Please enter (in degree Celsius) what angle you wish to have your shape rotate.");
+//Asking for the number in degrees that the user would like to rotate their shape
+
