fb7a5883258e3b7e66980946968c20377a85ec02
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-24 17:17:15 -0400

    added description to script.js file

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,3 +1,7 @@
 // Bryant, Samantha - 10/24/13 - Functions Assignment - Wacky
 
+// Wacky Function on the Movie Harry Potter
 
+// If there are 7 players on each Quidditch team, how many Howarts student Quidditch-players are there? (per year)
+
+// Given:
