1ffae21e0bee0df97e194d029151b3f4051eea3d
Samantha Bryant <samanthabryant@Samanthas-MacBook-Pro-10.local>
2013-10-24 21:34:17 -0400

    added variable for how many hours in a day to function
    var hoursInADay is the variable

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -12,12 +12,13 @@ var timesPerDay = prompt("How many times per day do you give her treats?");
 
 // To Calculate:
 
-var results = happyAriel(amountOfTreats, timesPerDay);
+var results = happyAriel(amountOfTreats, timesPerDay, 24);
 // To calculate the equation
 
 // Function and print to console:
 
 function happyAriel(amountOfTreats, timesPerDay){ //parameters for making a puppy happy
+    var hoursInADay = 24;
 
 }
 
